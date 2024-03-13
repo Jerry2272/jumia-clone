833ae80b349f84f5e0a93b783c1e65f8cb3affda
Jerry <jerryjake227@gmail.com>
2024-03-13 02:30:17 +0100

    update slick library

--- src/components/Hero/SlideShow/SlideShow.jsx
+++ src/components/Hero/SlideShow/SlideShow.jsx
@@ -1,8 +1,9 @@
 // Slider.js
 import React from 'react';
 import Slider from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.min.js'; 
 import './SlideShow.css'
 import { HSlide } from '../../../util/data';
 
--- src/components/Store/Store.jsx
+++ src/components/Store/Store.jsx
@@ -1,8 +1,9 @@
 // Slider.js
 import React from 'react';
 import Fade from 'react-slick';
-// import 'slick-carousel/slick/slick.css';
-// import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.min.js'; 
 import './Store.css';
 import { StoreAPI } from '../../util/data'
 // import { products } from '../../../util/data'; // Updated naming convention
